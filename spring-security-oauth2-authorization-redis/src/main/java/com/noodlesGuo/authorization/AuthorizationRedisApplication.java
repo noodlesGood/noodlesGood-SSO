5fbbbd59fa0027b1ec664a30bf9761d3ed228368
guode <1393694993@qq.com>
2019-10-29 14:08:19 +0900

    新建基于redis存储token的moudle

---
+++ spring-security-oauth2-authorization-redis/src/main/java/com/noodlesGuo/authorization/AuthorizationRedisApplication.java
@@ -0,0 +1,11 @@
+package com.noodlesGuo.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationRedisApplication.class,args);
+    }
+}
