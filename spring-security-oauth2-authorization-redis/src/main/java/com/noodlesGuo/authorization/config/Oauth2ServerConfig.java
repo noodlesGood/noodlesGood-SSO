8ee14e4d426fed321e4498621ae3b490d2dabdf2
guode <1393694993@qq.com>
2019-10-29 15:10:32 +0900

    添加redis配置，实现了将token存入redis

---
+++ spring-security-oauth2-authorization-redis/src/main/java/com/noodlesGuo/authorization/config/Oauth2ServerConfig.java
@@ -0,0 +1,69 @@
+package com.noodlesGuo.authorization.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+import javax.sql.DataSource;
+
+/**
+ * oauth2授权服务配置类
+ */
+@Configuration
+@EnableAuthorizationServer//开启授权服务
+public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;//配置数据源
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //基于内存的client
+//        InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+//        builder.withClient("client1")
+//                .secret("$2a$10$5ZqTPJbt6PBMmEWUCu7v7uqPapBJsdAE5qFcY2ZE5dnXcGOA1Age6")
+//                .resourceIds("oauth1")
+//                .authorizedGrantTypes("password","authorization_code","refresh_token")
+//                .authorities("ROLE_ADMIN","ROLE_USER")
+//                .scopes("all")
+//                .accessTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
+//                .refreshTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
+//                .redirectUris("http://www.baidu.com/");
+        //基于数据库查询，实现客户端的数据库存储
+        clients.withClientDetails(clientDetails());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(redisTokenStore());
+    }
+
+    /**
+     * 声明clientDetail的实现
+     * @return JdbcClientDetailsService
+     */
+    @Bean
+    public ClientDetailsService clientDetails(){
+        return new JdbcClientDetailsService(dataSource);
+    }
+
+    /**
+     * 声明 redisTokenStore 的实现 实现token存入redis
+     * @return RedisTokenStore
+     */
+    @Bean
+    public TokenStore redisTokenStore(){
+        return new RedisTokenStore(redisConnectionFactory);
+    }
+}
