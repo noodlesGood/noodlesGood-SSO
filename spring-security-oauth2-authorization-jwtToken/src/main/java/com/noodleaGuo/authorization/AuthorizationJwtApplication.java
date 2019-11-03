464cd478aab8d7a823070feb66f84d532fcafb9d
guode <1393694993@qq.com>
2019-11-03 15:46:02 +0800

    新建基于jwtToken的返回信息的Moudle

---
+++ spring-security-oauth2-authorization-jwtToken/src/main/java/com/noodleaGuo/authorization/AuthorizationJwtApplication.java
@@ -0,0 +1,11 @@
+package com.noodleaGuo.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationJwtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationJwtApplication.class,args);
+    }
+}
